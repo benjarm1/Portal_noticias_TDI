56e82f257534b56a9bc3c51f6f66318bdb6b599b
Mateo Garcia <mateonicolasgarcia2007@gmail.com>
2026-06-08 12:21:37 -0300

    Aca el js del login

---
+++ js/auth.js
@@ -0,0 +1,39 @@
+
+const form = document.querySelector("#loginForm");
+const errorText = document.querySelector("#error");
+
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  
+  const username = document.querySelector("#username").value;
+  const password = document.querySelector("#password").value;
+  
+  try {
+    const response = await fetch("https://dummyjson.com/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username,
+        password
+      })
+    });
+            
+        if (!response.ok) {
+      throw new Error("Credenciales incorrectas");
+    }
+
+    const data = await response.json();
+
+    
+    sessionStorage.setItem("admin", JSON.stringify(data));
+
+    
+    window.location.href = "admin.html";
+
+  } 
+  catch (error) {
+    alert("Error, contraseña o usuario incorrecto");
+  }
+});
