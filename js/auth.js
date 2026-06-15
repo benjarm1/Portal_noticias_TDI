07547ddb1259be21bafe3ece5a1798c3c6c61f62
Benjarm <146034526+benjarm1@users.noreply.github.com>
2026-06-15 11:54:15 -0300

    Arreglo login con sessionStorage

---
+++ js/auth.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("loginForm");
+
+  if (!form) return;
+
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    try {
+      const response = await fetch("https://dummyjson.com/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          username,
+          password
+        })
+      });
+
+      if (!response.ok) {
+        throw new Error("Credenciales incorrectas");
+      }
+
+      const data = await response.json();
+
+      sessionStorage.setItem("adminLogged", "true");
+      sessionStorage.setItem("adminUser", JSON.stringify(data));
+
+      window.location.href = "admin.html";
+    } catch (error) {
+      alert("Usuario o contraseña incorrectos");
+    }
+  });
+});
