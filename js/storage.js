90b2425503399832ed51aeb1e186c262c0d29601
Benjarm <146034526+benjarm1@users.noreply.github.com>
2026-06-15 11:52:16 -0300

    Agrego creacion de noticias desde admin.

---
+++ js/storage.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("newsForm");
+
+  if (!form) return;
+
+  const idInput = document.getElementById("newsId");
+  const titleInput = document.getElementById("title");
+  const descriptionInput = document.getElementById("description");
+  const imageInput = document.getElementById("image");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const title = titleInput.value.trim();
+    const description = descriptionInput.value.trim();
+    const image = imageInput.value.trim();
+
+    if (!title || !description || !image) {
+      alert("Completá todos los campos");
+      return;
+    }
+
+    const news = getNews();
+
+    const newNews = {
+      id: Date.now(),
+      title,
+      description,
+      image
+    };
+
+    news.push(newNews);
+    saveNews(news);
+
+    form.reset();
+
+    renderNews("adminNewsContainer", true);
+    renderNews("newsContainer", false);
+  });
+});
